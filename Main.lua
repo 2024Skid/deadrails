a8ee7e75e708686f31579c8ebe55eef03a03a51c
Legit <168124333+2024Skid@users.noreply.github.com>
2025-04-05 22:57:32 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,18 +1,1 @@
-local Player = game.Players.LocalPlayer
-local placeId = game.PlaceId
 
-local Games = {
-    [70876832253163] = httpsraw.githubusercontent.com2024Skidautowinrefsheadsmainloader.lua,
-}
-
-local scriptUrl = Games[placeId]
-
-if scriptUrl then
-    local suc, res = pcall(function()
-        return gameHttpGet(scriptUrl, true)
-    end)
-    
-    if suc then
-        loadstring(res)()
-    end
-end
