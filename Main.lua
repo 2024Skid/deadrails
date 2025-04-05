8844644ee38420e4a727df4f7b8b47617ea616fb
Legit <168124333+2024Skid@users.noreply.github.com>
2025-04-05 22:56:24 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,18 @@
-local v0=game.Players.LocalPlayer;local v1=game.PlaceId;local v2={[70876832253163]=httpsraw.githubusercontent.com2024Skidautowinrefsheadsmainloader.lua};local v3=v2[v1];if v3 then local v4=0;local v5;local v6;while true do if (v4==(0 -0)) then v5,v6=pcall(function() return gameHttpGet(v3,true);end);if v5 then loadstring(v6)();end break;end end end
+local Player = game.Players.LocalPlayer
+local placeId = game.PlaceId
+
+local Games = {
+    [70876832253163] = httpsraw.githubusercontent.com2024Skidautowinrefsheadsmainloader.lua,
+}
+
+local scriptUrl = Games[placeId]
+
+if scriptUrl then
+    local suc, res = pcall(function()
+        return gameHttpGet(scriptUrl, true)
+    end)
+    
+    if suc then
+        loadstring(res)()
+    end
+end
